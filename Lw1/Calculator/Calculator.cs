59820e98e71e056c1c5890a921c8147649f11a95
MishaNya <kholodm07@gmail.com>
2022-10-05 18:50:15 +0300

    Added labs

---
+++ Lw1/Calculator/Calculator.cs
@@ -0,0 +1,117 @@
+﻿namespace Calculator
+{
+    class Calculator
+    {
+        private double x, y;
+        private int operationKey;
+
+        public void InputOperation()
+        {
+            bool operationNotFound = true;
+            while (operationNotFound)
+            {
+                Console.WriteLine("Operations:\n" +
+                "0 for exit\n" +
+                "1 for + \n" +
+                "2 for - \n" +
+                "3 for * \n" +
+                "4 for / \n");
+                Console.WriteLine("Enter operation:");
+
+                try
+                {
+                    operationKey = Convert.ToInt32(Console.ReadLine());
+
+                    if (operationKey < 0 || operationKey > 4)
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Incorect operation number");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        if (operationKey == 0)
+                        {
+                            Console.WriteLine("Exiting...");
+                            System.Environment.Exit(1);
+                        }
+
+                        operationNotFound = false;
+                    }
+                }
+                catch
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Incorect operation format");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        public void InputOperands()
+        {
+            bool operandNotFound = true;
+            while (operandNotFound)
+            {
+                Console.WriteLine("Enter 1 operand: ");
+
+                try
+                {
+                    x = Convert.ToDouble(Console.ReadLine());
+                    operandNotFound = false;
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Incorect number format");
+                    Console.ResetColor();
+                }
+            }
+
+            operandNotFound = true;
+            while (operandNotFound)
+            {
+                Console.WriteLine("Enter 2 operand: ");
+
+                try
+                {
+                    y = Convert.ToDouble(Console.ReadLine());
+                    operandNotFound = false;
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Incorect number format");
+                    Console.ResetColor();
+                }
+            }
+        }
+        public void PerformOperation()
+        {
+            switch(operationKey)
+            {
+                case 1:
+                    Console.WriteLine(x + " + " + y + " = " + (x + y));
+                    break;
+                case 2:
+                    Console.WriteLine(x + " - " + y + " = " + (x - y));
+                    break;
+                case 3:
+                    Console.WriteLine(x + " * " + y + " = " + (x * y));
+                    break;
+                case 4:
+                    if (y != 0)
+                    {
+                        Console.WriteLine(x + " / " + y + " = " + (x / y));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Can't divide by zero");
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ Lw1/Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Calculator calc = new Calculator();
+            while (true)
+            {
+                Console.Clear();
+                calc.InputOperation();
+                calc.InputOperands();
+                calc.PerformOperation();
+                Console.WriteLine("Press anything to repeat");
+                Console.ReadLine();
+            }
+        }
+    }
+}
